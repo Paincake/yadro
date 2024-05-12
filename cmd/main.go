b79ad6e5a015000c523445d1d0576a082830e34d
Paincake <ryazhev2012@gmail.com>
2024-05-12 20:12:10 +0300

    cli

--- cmd/main.go
+++ cmd/main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Paincake/yadro/event"
 	"os"
 )
 
-//TODO add cli
+//TODO testing
+//TODO restructure
+//TODO start and end processing printing
+//TODO better processing loop
 
 func main() {
-	f, _ := os.Open("C:\\Users\\ryazh\\GolandProjects\\yadro\\test")
+	if len(os.Args) < 2 {
+		fmt.Println("usage: <task.exe> <filename>")
+		return
+	}
+	filename := os.Args[1]
+	f, _ := os.Open(filename)
 	src, err := event.NewClubFileSource(f)
 	if err != nil {
 		panic(err)
 	}
 
 	processor := event.NewProcessor(src, os.Stdout)
-	//TODO
+	//TODO this is so bad
 	processor.Club = src.Club
 
 	for {
